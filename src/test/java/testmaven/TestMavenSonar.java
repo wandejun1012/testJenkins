fd71ed956975c94bddc0d611694e47cfd8222ce7
wandejun <wandejun@nonobank.com>
2016-03-10 20:28:25 +0800

    add test case

---
+++ src/test/java/testmaven/TestMavenSonar.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2009-2015. 上海诺诺镑客 All rights reserved.
+ * @(#) TestMaven.java 2016年3月10日 下午8:25:05
+ */
+package testmaven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @FileName: TestMaven.java
+ * @Function:TODO
+ * @date: 2016年3月10日 下午8:25:05
+ * @author: wandejun
+ * @since JDK 1.8
+ */
+public class TestMavenSonar {
+
+	@Test
+	public void testMavenSonar(){
+		String a="aaa";
+		String b="bbb";
+		Assert.assertTrue(a.length()==b.length());
+	}
+}
