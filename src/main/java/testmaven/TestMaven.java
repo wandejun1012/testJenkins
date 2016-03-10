2054303b68ab89007e3bef0d95918d681320c453
wandejun <wandejun@nonobank.com>
2016-03-10 18:40:30 +0800

    first commit

---
+++ src/main/java/testmaven/TestMaven.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2009-2015. 上海诺诺镑客 All rights reserved.
+ * @(#) TestMaven.java 2016年3月10日 上午11:36:51
+ */
+package testmaven;
+/**
+ * @FileName: TestMaven.java
+ * @Function:TODO
+ * @date: 2016年3月10日 上午11:36:51
+ * @author: wandejun
+ * @since JDK 1.8
+ */
+public class TestMaven {
+
+	public static void main(String[] args) {
+		System.out.println("i am maven");
+		String a=null;
+		a.toCharArray();
+	}
+
+}
